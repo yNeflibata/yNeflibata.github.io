266f2cd97ad29ffccfc32c56cb0d4026674f5a46
yNeflibata <2795754784@qq.com>
2024-08-10 22:17:33 +0800

    Site updated: 2024-08-10 22:17:29

--- js/visitor.js
+++ js/visitor.js
@@ -222,4 +222,4 @@ function showWelcome() {
 }
 window.onload = showWelcome;
 // 如果使用了pjax在加上下面这行代码
-document.addEventListener('pjax:complete', showWelcome);
+// document.addEventListener('pjax:complete', showWelcome);
